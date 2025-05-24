14a02cda5eeb0ab2bde22bb4452c8c253d45dc8f
Susumu Yamazaki <zacky1972@gmail.com>
2025-05-25 02:06:02 +0900

    initial commit

---
+++ lib/epmd_up.ex
@@ -0,0 +1,18 @@
+defmodule EpmdUp do
+  @moduledoc """
+  Documentation for `EpmdUp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EpmdUp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/epmd_up_test.exs
@@ -0,0 +1,8 @@
+defmodule EpmdUpTest do
+  use ExUnit.Case
+  doctest EpmdUp
+
+  test "greets the world" do
+    assert EpmdUp.hello() == :world
+  end
+end
