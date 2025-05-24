77eebeed80cfe4ebe379ad225130ba3f715f6e35
Susumu Yamazaki <zacky1972@gmail.com>
2025-05-25 02:21:43 +0900

    Downgrade Elixir version requirement to 1.15
    - Changed Elixir version requirement from ~> 1.18 to ~> 1.15 in mix.exs
    - This change makes the project compatible with Elixir 1.15.x series

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EpmdUp.MixProject do
     [
       app: :epmd_up,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
