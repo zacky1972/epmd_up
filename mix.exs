3691494faa41c421fc1f9a3530d5cde6558d4582
Susumu Yamazaki <zacky1972@gmail.com>
2025-05-25 22:15:49 +0900

    build: Add documentation generation with ex_doc
    - Add ex_doc dependency for generating documentation
    - Configure documentation settings in mix.exs
    - Set up README.md as an extra document
    - Add required dependencies for documentation generation

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule EpmdUp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "EpmdUp",
+      source_url: "http://github.com/zacky1972/epmd_up",
+      docs: &docs/0
     ]
   end
 
@@ -23,7 +28,15 @@ defmodule EpmdUp.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "EpmdUp",
+      extras: ["README.md"]
     ]
   end
 end
