304f63ba3248d83ca7fb8778922eaae75b1eed0c
Susumu Yamazaki <zacky1972@gmail.com>
2025-06-28 19:28:07 +0900

    chore: bump version to 1.0.2
    Update package version for patch release.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule EpmdUp.MixProject do
   def project do
     [
       app: :epmd_up,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
